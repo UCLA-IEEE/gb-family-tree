f16614435670840db1a76d244c8934313001b495
Robert Zalog <rzalog@gmail.com>
2019-02-25 13:38:49 -0800

    Fix year filters.

--- js/data.js
+++ js/data.js
@@ -4,16 +4,16 @@ const SPREADSHEET_ID = '16GxwOgUyLDTbeukERrZKlAZV_JumFUJH53PTafrNcIo'
 const API_BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/'
 
 const API_OPTIONS = '/values/Groups!A2:J1000?key='
-// API_KEY provided in separate file key.js so that we don't have to push
+// API_KEY provided in separate file secrets.js so that we don't have to push
 // it to GitHub
 
 function rowToGroup(row) {
     // Relies on the following column structure in the Google Sheet:
-    // GB Name | Lead 1 | Lead 2 | Member 1 | Member 2 | ... | Member 6
+    // GB Name | Lead 1 | Lead 2 | Year | Member 1 | Member 2 | ... | Member 6
     
     console.log('convert ' + row)
 
-    if (!row[0] || !row[2] || !row[3]) {
+    if (!row[0] || !row[2] || !row[3] || !row[4]) {
         return null
     }
 
@@ -22,8 +22,9 @@ function rowToGroup(row) {
     group.name = row[0]
     group.leads = [row[1]]
     if (row[2]) group.leads.push(row[2])
+    group.year = row[3]
     group.members = []
-    for (var i=3; i < row.length; i++)
+    for (var i=4; i < row.length; i++)
         group.members.push(row[i])
 
     return group
